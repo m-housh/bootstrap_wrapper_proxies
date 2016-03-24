475696752931f53458d5b895d06e7cf325450895
Michael Housh <mhoush@houshhomeenergy.com>
2016-03-24 08:48:20 -0400

    Added utils_test.py

--- proxies/proxies/__init__.py
+++ proxies/proxies/__init__.py
@@ -1,7 +1,7 @@
 from .schema_key import SchemaKey, SchemaMap, SchemaLabelProtocol
 from .model_view import ModelViewProxy
 from .core import BaseDict, BaseChainMap, BaseViewContext
-from .utils import OrderedLabels, NamedTupleHelper
+from .utils import OrderedLabels
 
 __all__ = ['SchemaKey', 'SchemaMap', 'SchemaLabelProtocol', 'ModelViewProxy', 'OrderedLabels', \
          'BaseDict', 'BaseChainMap', 'BaseViewContext' ,]
